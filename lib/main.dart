aa9ede9bcbdf828effa4ce93a17ef773200c9aa8
Ido Hirsh <ido.hirsh.microsoft@gmail.com>
2021-02-09 20:20:52 +0200

    ver1.3

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Chat',
+      title: 'Blackspace',
       theme: ThemeData(
         primaryColor: Colors.black,
         backgroundColor: Colors.black,
